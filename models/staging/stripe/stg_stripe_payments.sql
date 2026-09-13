5c06f8e995b4e165750145930d8b29c3cdd8ae60
Gabriel Bressan <gabriel.del.santoro@hotmail.com>
2026-09-13 14:14:47 -0400

    sources

--- models/staging/stripe/stg_stripe_payments.sql
+++ models/staging/stripe/stg_stripe_payments.sql
@@ -1,9 +1,9 @@
-select 
-  id as payment_id,
-  orderid as order_id,
-  paymentmethod as payment_method,
-  status,
-  amount/100 as amount,
-  created as payment_date
-from 
-  `dbt-tutorial`.`stripe`.`payment`
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+from {{ source('stripe', 'payment') }}
